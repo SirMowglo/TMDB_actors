8ce37eefb548c23e2a6fb82d590d255dedc89569
AlejandroDV <aflowv@hotmail.com>
2022-10-29 18:26:19 +0200

    Animation known for

--- src/app/dialogs/actor-details-diag/actor-details-diag.component.ts
+++ src/app/dialogs/actor-details-diag/actor-details-diag.component.ts
@@ -10,9 +10,12 @@ import { ActorDialogData } from 'src/app/interfaces/actorsDiagData.interface';
   styleUrls: ['./actor-details-diag.component.css'],
 })
 export class ActorDetailsDiagComponent implements OnInit {
+  moviesList: string[]= [];
   constructor(@Inject(MAT_DIALOG_DATA) public data: ActorDialogData) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getActorMovies(this.data.actor);
+  }
   getActorImage(actor: Actor) {
     let url = '';
     if (actor.profile_path != null) {
@@ -25,5 +28,10 @@ export class ActorDetailsDiagComponent implements OnInit {
 
     truncado = Math.trunc(num);
     return truncado;
+  }
+  getActorMovies(actor: Actor) {
+    for(var i =0;i < actor.known_for.length; i++){
+      this.moviesList.push(actor.known_for[i].poster_path); 
+    }
   }
 }
